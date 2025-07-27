b5e34ca89a986cdd3e03f11c004863e0ab06f6de
SangWon7242 <gotkdqja@gmail.com>
2025-07-27 12:29:17 +0900

    api key를 환경변수를 통해 관리

--- firebase/config.ts
+++ firebase/config.ts
@@ -7,7 +7,7 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "---",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: "simplepostapp2.firebaseapp.com",
   projectId: "simplepostapp2",
   storageBucket: "simplepostapp2.firebasestorage.app",
