38cff5cf420b14abe722f3ef1eab5003d270a518
SangWon7242 <gotkdqja@gmail.com>
2025-07-27 12:20:38 +0900

    step3 : 인터페이스 도입, 상속을 통한 중복 제거

--- app/(tabs)/posts/page.tsx
+++ app/(tabs)/posts/page.tsx
@@ -1,5 +1,4 @@
-// type : 타입스크립트에서 타입을 정의할 때 사용
-import type { PostDto } from "@/types/post";
+import { PostDto } from "@/types/post";
 import { Link } from "expo-router";
 import { useEffect, useState } from "react";
 import { Dimensions, FlatList, StyleSheet, Text, View } from "react-native";
--- types/post.ts
+++ types/post.ts
@@ -1,3 +1,4 @@
+/*
 export type PostDto = {
   userId: number;
   id: number;
@@ -5,9 +6,16 @@ export type PostDto = {
   body: string;
 };
 
-export type PostWithContentDto = {
+// PostDto & {}; : PostDto의 타입을 유지하면서 추가적인 프로퍼티를 추가할 수 있다.
+
+export type PostWithContentDto = PostDto & {};
+*/
+
+export interface PostDto {
   userId: number;
   id: number;
   title: string;
   body: string;
-};
+}
+
+export interface PostWithContentDto extends PostDto {}
